d6fc700c83e02d886e41fab1a666f55e6608ccde
Andrey Barabash <andrey.barabash@ebuyer.com>
2019-02-12 10:24:13 +0300

    Modify comment field validation

--- app/code/Barabash/Checkout/view/frontend/web/js/custom-checkbox.js
+++ app/code/Barabash/Checkout/view/frontend/web/js/custom-checkbox.js
@@ -2,28 +2,31 @@ define([
     'jquery'
 ], function ($) {
 
-    var $comment = $('textarea[name="custom_attributes[customer_comment]"]');
-    var $checkboxLabel = $(document.body).find('.custom-comment-checkbox').closest('.field').find('label.label > span');
+    var $comment        = $('textarea[name="custom_attributes[customer_comment]"]');
+    var $checkboxLabel  = $(document.body).find('.custom-comment-checkbox').closest('div.field').find('label.label > span');
+    var currentUrl      = location.href;
+    const LABEL_ENABLE  = 'Enable';
+    const LABEL_DISABLE = 'Disable';
 
     if ($comment.val().length === 0) {
         $(document.body).find('.custom-comment-checkbox').prop('checked', false);
-        $checkboxLabel.text('Enable');
+        $checkboxLabel.text(LABEL_ENABLE);
         $comment.closest('div.field').hide();
     } else {
-        $checkboxLabel.text('Disable');
+        $checkboxLabel.text(LABEL_DISABLE);
     }
-    $comment.closest('.field').addClass('_required');
+    $comment.closest('div.field').addClass('_required');
 
     $(document.body).on('click', '.custom-comment-checkbox', function() {
         if ($(this).is(':checked')) {
             $comment.closest('div.field').show();
             $comment.removeClass('hidden').addClass('visible').show();
-            $checkboxLabel.text('Disable');
-            $comment.closest('.field').addClass('_required');
+            $checkboxLabel.text(LABEL_DISABLE);
+            $comment.closest('div.field').addClass('_required');
         } else {
             $comment.closest('div.field').hide();
             $comment.removeClass('visible').addClass('hidden').hide();
-            $checkboxLabel.text('Enable');
+            $checkboxLabel.text(LABEL_ENABLE);
             $comment.val('');
         }
     });
@@ -34,21 +37,19 @@ define([
     });
 
     $('form').on('submit', function(e) {
-        if ($comment.hasClass('visible') && $comment.val().length === 0) {
-            commentNeedToBeFilled($comment);
+        if ($(document.body).find('.field-error').length > 0) {
             e.preventDefault();
+            location.href = currentUrl;
         }
     });
 
     $('button[type=submit]').on('click', function(e) {
         if ($comment.hasClass('visible') && $comment.val().length === 0) {
-            commentNeedToBeFilled($comment);
-            e.preventDefault();
+            $comment.css('border', '1px solid red');
+            if ($comment.closest('div').find('.field-error').length > 0) {
+                return false;
+            }
+            $comment.closest('div').append($('<span />').addClass('field-error').text('This is a required field.'));
         }
     });
-
-    function commentNeedToBeFilled($comment) {
-        $comment.css('border', '1px solid red');
-        $comment.closest('div').append($('<span />').addClass('field-error').text('This is a required field.'));
-    }
 });
